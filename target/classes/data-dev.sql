1f90743704fbc03d47ca8d59fe35af70724deede
AUraihQA <AUraih@qa.com>
2021-02-15 18:53:54 +0000

    Target folder added

---
+++ target/classes/data-dev.sql
@@ -0,0 +1,10 @@
+INSERT INTO LIST_DOMAIN (NAME) 
+VALUES 
+('Work'), 
+('Personal');
+
+INSERT INTO TO_DO_DOMAIN (DESCRIPTION, DATE_CREATED, DEADLINE_DATE, COMPLETION, MY_LIST_ID) 
+VALUES 
+('Finish Task 1', '11th February 2021', '21st February 2021', 'No', 1), 
+('Finish Task 2', '21th February 2021', '22nd February 2021', 'No', 1), 
+('Personal Task', '11th March 2021', '21st February 2022', 'No', 2);
